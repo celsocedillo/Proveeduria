8cb03383468615653d437edd4faf6342bb936dc8
Celso Cedillo Flores <ccedillo@emapag-ep.gob.ec>
2018-04-27 11:58:32 -0500

    Uso de Ajax en load
    Se utiliza Ajax para cargar los datos y se agrega el efecto de load

--- Proveeduria/WebApplicationMvc/Controllers/TipoMovimientoController.cs
+++ Proveeduria/WebApplicationMvc/Controllers/TipoMovimientoController.cs
@@ -7,12 +7,15 @@ using Proveduria.Models;
 using Proveduria.Repositories;
 using System.Net;
 using System.Data.Entity;
+using Newtonsoft.Json.Linq;
+using NLog;
 
 namespace Proveduria.Controllers
 {
     public class TipoMovimientoController : Controller
     {
         private UnitOfWork unitOfWork = new UnitOfWork();
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         // GET: TipoMovimiento
         public ActionResult ListaTipoMovimiento()
         {
@@ -34,7 +37,37 @@ namespace Proveduria.Controllers
                 return Json(new { resultado = "error", data = "", mensaje = " Error al consultar el tipo de movimiento, favor revisar las conecciones de base de datos => [" + ex + "]" });
             }
         }
-   
+
+        [HttpPost]
+        public ActionResult GetListaTipoMovimiento()
+        {
+            JArray jArray = new JArray();
+            JObject total = new JObject();
+            try
+            {
+                var query = from d in unitOfWork.TipoMovimientoRepository.GetAll()
+                            select d;
+                foreach (EPRTA_TIPO_MOVIMIENTO item in query)
+                {
+                    JObject jsonObject = new JObject
+                    {
+                        { "ID_TIPO_MOVIMIENTO", item.ID_TIPO_MOVIMIENTO },
+                        { "NOMBRE", item.NOMBRE }
+                    };
+                    jArray.Add(jsonObject);
+                }
+                total = new JObject();
+                total.Add("items", jArray);
+                total.Add("error", false);
+            }
+            catch (Exception ex)
+            {
+                total.Add("error", true);
+                logger.Error(ex, ex.Message);
+            }
+            return Content(total.ToString(), "application/json");
+        }
+
 
     }
 }
---
+++ Proveeduria/WebApplicationMvc/Scripts/App/Util.js
@@ -0,0 +1,16 @@
+﻿function run_waitMe(el, effect, texto) {
+    fontSize = '';
+    maxSize = '';
+    textPos = 'vertical';
+    el.waitMe({
+        effect: effect,
+        text: texto,
+        bg: 'rgba(255,255,255,0.7)',
+        color: '#000',
+        maxSize: 30,
+        source: 'img.svg',
+        textPos: textPos,
+        fontSize: fontSize,
+        onClose: function () { }
+    });
+}
